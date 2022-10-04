78fc68f1003ee451cdfb806a91cd8f256adf63ae
Cid R. Andrade <profandrade@gmail.com>
2022-10-04 17:35:41 -0300

    Versão 1.0.22.10.04 Incorpora biblioteca pessoal

--- src/br/com/cidandrade/aulas/PojoApp.java
+++ src/br/com/cidandrade/aulas/PojoApp.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package br.com.cidandrade.aulas;
+
+import br.com.cidandrade.util.Entrada;
+import br.com.cidandrade.util.Mensagem;
 
 /**
  *
@@ -14,10 +13,21 @@ public class PojoApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Triangulo triangulo = new Triangulo(4.0, 5.0);
-        Retangulo retangulo = new Retangulo(4.0, 5.0);
-        System.out.println(triangulo.getDescricao());
-        System.out.println(retangulo.getDescricao());
+        try {
+            float medida1 = Entrada.getFloat("Digite uma medida");
+            float medida2 = Entrada.getFloat("Digite outra medida");
+
+            Triangulo triangulo = new Triangulo(medida1, medida2);
+            Retangulo retangulo = new Retangulo(medida1, medida2);
+
+            System.out.println(triangulo.getDescricao());
+            System.out.println(retangulo.getDescricao());
+        } catch (NumberFormatException e) {
+            Mensagem.msgErro("Erro na entrada de dados.\n"
+                    + e.getLocalizedMessage());
+        } finally {
+            System.exit(0);
+        }
     }
 
 }
